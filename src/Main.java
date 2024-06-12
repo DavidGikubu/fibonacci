a50e2e2bb722b5e66f16b6c7ea78da56c9eb34de
david <davidndungu8203@gmail.com>
2024-06-12 22:45:56 +0300

    this program calculates the Nth Fibonacci number accordingly.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //It takes input N from the user and calculates the Nth Fibonacci number accordingly
+        System.out.println("welcome to the fibonacci series");
+        Scanner scanner = new Scanner(System.in);//creating a scanner object to take users input.
+        int number = scanner.nextInt();//reading the user input for the position of the fibonacci number to find
+
+        int firstNumber = 0;//the first fibonacci number
+        int secondNumber = 1;//the second fibonacci number
+
+
+
+
+    }
+}
