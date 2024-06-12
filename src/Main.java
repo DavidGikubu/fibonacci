865d2b5a1cbe91bb007584b99d39af03784a1c54
david <davidndungu8203@gmail.com>
2024-06-12 23:13:48 +0300

    the fibonacci program is done and functional,,smiley face (^_^)

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //It takes input N from the user and calculates the Nth Fibonacci number accordingly
-        System.out.println("welcome to the fibonacci series");
+        //It takes input Number from the user and calculates the Nth Fibonacci number accordingly
+        System.out.println("welcome to the fibonacci series: give me a number: ");
+
         Scanner scanner = new Scanner(System.in);//creating a scanner object to take users input.
         int number = scanner.nextInt();//reading the user input for the position of the fibonacci number to find
 
         int firstNumber = 0;//the first fibonacci number
         int secondNumber = 1;//the second fibonacci number
-        //handling corner cases when N is 0 or negative
+        //handling corner cases when Number is 0 or negative
         if(number <=0){
             System.out.println("The number can never be zero or negative");/*printing the error message*/
             return; //exiting the program
@@ -24,7 +25,17 @@ public class Main {
         }
         //if the number is greater than 2 calculate the number iteratively
         else {
+            //variable to hold the current fibonacci number
+            int current = 0;
+            //loop for the Nth fibonacci number
+            for (int j = 3; j<=number; j++ ){
+                current= firstNumber +secondNumber;//calculating the current fibonacci number
+                firstNumber = secondNumber;//updating the current fibonacci number
+                secondNumber = current;//updating the second fibonacci number
 
+            }
+            //printing the Nth fibonacci number
+            System.out.println("The "+ number +"th fibonacci number is : " + current);
         }
 
 
